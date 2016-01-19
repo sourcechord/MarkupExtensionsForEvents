fd90a500bc9b7055707d0ccb829ec57c5bc3c7b4
minami <sourcechord@gmail.com>
2016-01-20 01:00:14 +0900

    Initial Commit
    Moved from CodePlex to GitHub.

---
+++ MarkupExtensionsForEvents/InvokeCommandExtension.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace MarkupExtensionsForEvents
+{
+    [MarkupExtensionReturnType(typeof(EventHandler))]
+    public sealed class InvokeCommandExtension : MarkupExtension
+    {
+        /// <summary>
+        /// イベント発生時に呼び出すコマンドのパスを取得または設定します。
+        /// </summary>
+        public PropertyPath Path { get; set; }
+
+        private TargetObject _targetObject;
+
+        public InvokeCommandExtension(PropertyPath bindingCommandPath)
+        {
+            this.Path = bindingCommandPath;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var pvt = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
+
+            if (pvt != null)
+            {
+                var ei = pvt.TargetProperty as EventInfo;
+                var mi = pvt.TargetProperty as MethodInfo;
+                var type = (ei != null) ? ei.EventHandlerType :
+                                          (mi != null) ? mi.GetParameters()[1].ParameterType :
+                                                         null;
+
+                if (type != null)
+                {
+                    var target = pvt.TargetObject as FrameworkElement;
+
+                    _targetObject = new TargetObject();
+                    this.SetBinding(target.DataContext);
+                    target.DataContextChanged += (s, e) => { this.SetBinding(e.NewValue); };
+
+
+                    // ここで、イベントハンドラを作成し、マークアップ拡張の結果として返す
+                    var nonGenericMethod = GetType().GetMethod("PrivateHandlerGeneric", BindingFlags.NonPublic | BindingFlags.Instance);
+                    var argType = type.GetMethod("Invoke").GetParameters()[1].ParameterType;
+                    var genericMethod = nonGenericMethod.MakeGenericMethod(argType);
+
+                    return Delegate.CreateDelegate(type, this, genericMethod);
+                }
+            }
+
+            return null;
+        }
+
+        private void SetBinding(object dataContext)
+        {
+            var binding = new Binding()
+            {
+                Source = dataContext,
+                Path = this.Path,
+            };
+            BindingOperations.SetBinding(_targetObject, TargetObject.TargetValueProperty, binding);
+        }
+
+        private void PrivateHandlerGeneric<T>(object sender, T e)
+        {
+            // コマンドの取得
+            var command = _targetObject.TargetValue as ICommand;
+
+            // コマンドを呼び出す
+            if (command != null && command.CanExecute(e))
+            {
+                command.Execute(e);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 実行対象のコマンドをバインディングターゲットとして保持するために使用するクラス
+    /// InvokeCommandExtensionクラス内で使用します
+    /// </summary>
+    internal class TargetObject : DependencyObject
+    {
+        public object TargetValue
+        {
+            get { return (object)GetValue(TargetValueProperty); }
+            set { SetValue(TargetValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TargetValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TargetValueProperty =
+            DependencyProperty.Register("TargetValue", typeof(object), typeof(TargetObject), new PropertyMetadata(null));
+    }
+}
---
+++ MarkupExtensionsForEvents/MainWindowViewModel.cs
@@ -0,0 +1,113 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MarkupExtensionsForEvents
+{
+    /// <summary>
+    /// MainWindow.xamlに使用するViewModel
+    /// </summary>
+    class MainWindowViewModel : BindableBase
+    {
+        private string imagePath;
+        /// <summary>
+        /// 表示する画像のパスを取得または設定します。
+        /// </summary>
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set { this.SetProperty(ref this.imagePath, value); }
+        }
+
+
+
+        private ObservableCollection<ItemViewModel> list;
+        public ObservableCollection<ItemViewModel> List
+        {
+            get { return list; }
+            set { this.SetProperty(ref this.list, value); }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            this.List = new ObservableCollection<ItemViewModel>()
+            {
+                new ItemViewModel(){Name="hoge1", Value=10},
+                new ItemViewModel(){Name="hoge2", Value=20},
+                new ItemViewModel(){Name="hoge3", Value=30},
+                new ItemViewModel(){Name="hoge4", Value=40},
+                new ItemViewModel(){Name="hoge5", Value=50},
+            };
+        }
+
+        ~MainWindowViewModel()
+        {
+            System.Diagnostics.Trace.WriteLine("MainWindowViewModel Destructed.");
+        }
+
+        #region コマンドの実装
+        private RelayCommand showMessageCommand;
+        public RelayCommand ShowMessageCommand
+        {
+            get { return showMessageCommand = showMessageCommand ?? new RelayCommand(ShowMessage); }
+        }
+        private void ShowMessage()
+        {
+            MessageBox.Show("ShowMessage Command Invoked!!");
+        }
+
+        private RelayCommand<DragEventArgs> dropFileCommand;
+        public RelayCommand<DragEventArgs> DropFileCommand
+        {
+            get { return dropFileCommand = dropFileCommand ?? new RelayCommand<DragEventArgs>(DropFile); }
+        }
+
+        private void DropFile(DragEventArgs parameter)
+        {
+            var fileInfos = parameter.Data.GetData(DataFormats.FileDrop) as string[];
+            if (fileInfos != null)
+            {
+                // ドロップされたファイルの1番目の要素のパスを、画面表示用のプロパティに設定
+                this.ImagePath = fileInfos[0];
+            }
+        }
+        #endregion
+    }
+
+
+    class ItemViewModel : BindableBase
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { this.SetProperty(ref this.name, value); }
+        }
+
+        private int value;
+        public int Value
+        {
+            get { return value; }
+            set { this.SetProperty(ref this.value, value); }
+        }
+
+
+        private RelayCommand showValueCommand;
+        public RelayCommand ShowValueCommand
+        {
+            get { return showValueCommand = showValueCommand ?? new RelayCommand(ShowValue); }
+        }
+
+        private void ShowValue()
+        {
+            var msg = string.Format("{0}: {1}", this.Name, this.Value);
+            MessageBox.Show(msg);
+        }
+    }
+}
